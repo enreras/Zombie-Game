f2c0b93a9f79c9ea20966db6a0035637dc8304cc
ian contreras <enreras09@gmail.com>
2017-12-18 15:16:11 -0800

    initial commit

---
+++ js/zombie.js
@@ -0,0 +1,28 @@
+//variables 
+var room = [0, 1, 2];
+var room1 = [0, 1, 2];
+var room2 = [0, 1, 2];
+
+
+//constants
+var zombieImg = 'images/jumping-zombie.jpg';
+var doorImg = 'images/door.jpg';
+//Event listener
+document.getElementById("room").addEventListener("click", openDoor);
+
+//functions
+// how do we put a zombie behind a random door
+function openDoor(evt) {
+//    var num = Math.floor((Math.random() * 3) + 1);
+//    if(num < 2) {
+    room[1]=== zombieImg
+   }
+
+
+    // alert("clicked door" + evt)
+
+
+// 3 round of 3 doors 
+// 3 divs with class name of round 1, 2, 3, 
+// if you surive hide div of that round using z index or display: none 
+
